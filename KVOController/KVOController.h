4cf9b035020d7efe391e2cd3da9f323bb15aa34e
Kyle Levin <me@kylelevin.com>
2015-07-22 10:28:03 -0400

    moved to dynamic framework

---
+++ KVOController/KVOController.h
@@ -0,0 +1,21 @@
+//
+//  KVOController.h
+//  KVOController
+//
+//  Created by Kyle Levin on 7/22/15.
+//  Copyright © 2015 Kimon Tsinteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVOController.
+FOUNDATION_EXPORT double KVOControllerVersionNumber;
+
+//! Project version string for KVOController.
+FOUNDATION_EXPORT const unsigned char KVOControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVOController/PublicHeader.h>
+
+
+#import "FBKVOController.h"
+#import "NSObject+FBKVOController.h"
